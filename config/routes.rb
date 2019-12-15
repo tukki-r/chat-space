fcbb6dd3d196b4e4d6cbbaf03cd1eaaecb6fb33d
tukki-r <57993375+tukki-r@users.noreply.github.com>
2019-12-15 14:39:57 +0900

    ユーザー管理実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'messages/index'
+  resources :users, only: [:edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "messages#index" #message_controllerのindexアクションが動く

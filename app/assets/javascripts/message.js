f494c2d9e39a551b28fc33e539c81a0c503d083b
tukki-r <57993375+tukki-r@users.noreply.github.com>
2019-12-31 11:51:15 +0900

    jsonでHTMLを作成②

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,23 +2,44 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       var html =
-                `<div class="chat-main__messages__contents-data">
-                <div class="chat-main__messages__contents-data__update-date">
-                <div class="chat-main__messages__contents-data__update-date__name">
-                  ${message.user_name}
-                </div>
-                <div class="chat-main__messages__contents-data__update-date__date">
-                  ${message.created_at}
-                </div>
-                </div>
-                <div class="chat-main__messages__contents-data__messages-text">
-                ${message.content}
-                <img class="lower-message__image" src=${message.image} >
-                </div>
-                </div>`
+        `<div class="chat-main__messages__contents-data">
+        <div class="chat-main__messages__contents-data__update-date">
+        <div class="chat-main__messages__contents-data__update-date__name">
+          ${message.user_name}
+        </div>
+        <div class="chat-main__messages__contents-data__update-date__date">
+          ${message.created_at}
+        </div>
+        </div>
+        <div class="chat-main__messages__contents-data__messages-text">
+        <p class="message-text">
+          ${message.content}
+        </p>
+        <img class="lower-message__image" src=${message.image}>
+        </div>
+        </div>`
       return html;
+    }
+      else{
+        var html =
+          `<div class="chat-main__messages__contents-data">
+          <div class="chat-main__messages__contents-data__update-date">
+          <div class="chat-main__messages__contents-data__update-date__name">
+          ${message.user_name}
+          </div>
+          <div class="chat-main__messages__contents-data__update-date__date">
+          ${message.created_at}
+          </div>
+          </div>
+          <div class="chat-main__messages__contents-data__messages-text">
+          <p class="message-text">
+            ${message.content}
+          </p>
+          </div>
+          </div>`
+        return html;
+      };
   }
-    
 $('#new_message').on('submit', function(e){
   e.preventDefault();
   var formData = new FormData(this);
@@ -31,10 +52,8 @@ $('#new_message').on('submit', function(e){
     processData: false,
     contentType: false
   })
-.done(function(data){
-  var html = buildHTML(data);
-
-//   $('.message').append(html);
-//   $('.textbox').val('');
-//   $('.form__submit').prop('disabled',false);
-// }
+    .done(function(data){
+      var html = buildHTML(data);
+    })
+})
+});

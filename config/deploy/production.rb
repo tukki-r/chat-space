77fdca9b15d4b1e94a5d5ce68b0d990dca828734
tukki-r <57993375+tukki-r@users.noreply.github.com>
2020-01-07 18:07:04 +0900

    自動デプロイ２

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,8 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-
-
+server '52.197.73.108', user: 'ec2-user', roles: %w{app db web}
 
 # role-based syntax
 # ==================
